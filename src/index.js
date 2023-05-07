4b27ca55a186dc401552834b186a245b58da6c52
Evgeniy Shapechko <shapen86@mail.ru>
2023-05-07 13:13:08 +0300

    day 2 add style carrer and slider

--- src/index.js
+++ src/index.js
@@ -7,12 +7,36 @@ import { slidersInit } from "./modules/sliders";
 
 // use modules
 
-slidersInit(".about__slider"),
-    {
-        pagination: {
-            el: ".about__slider-pagination",
+slidersInit(".about__slider", {
+    pagination: {
+        el: ".about__slider-pagination",
+    },
+});
+
+slidersInit(".carrer__slider", {
+    pagination: {
+        el: ".carrer__slider-pagination",
+    },
+    breakpoints: {
+        768: {
+            slidesPerView: "auto",
+            spaceBetween: 20,
+            pagination: false,
         },
-    };
+
+        1024: {
+            slidesPerView: "auto",
+            spaceBetween: 26,
+            pagination: false,
+        },
+
+        1240: {
+            slidesPerView: "auto",
+            spaceBetween: 30,
+            pagination: false,
+        },
+    },
+});
 
 const videoBG = document.querySelector(".video-bg");
 
